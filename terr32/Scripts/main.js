91c3a3a0013f60316f0fa7df697230605876761f
ailo21 <alex92ailo@mail.ru>
2018-06-21 19:05:51 +0300

    start project

---
+++ terr32/Scripts/main.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    //if ($(".slider_pager_on").length > 0) {
+    //    $(".slider_pager_on").carousel({ interval: 6500 });
+
+
+    //    //устанавливаем жесткий размер обертки слайдера
+    //    $(".slider_pager_on").each(function () {
+    //        var SlideWr = $(this);
+    //        var maxHeight = 0;
+    //        $(this).find('.item').each(function () {
+    //            var HeightThis = $(this).outerHeight();
+    //            if (HeightThis > maxHeight) maxHeight = HeightThis;
+    //        });
+    //        SlideWr.find('.item').css('min-height', maxHeight);
+    //    });
+
+    //}
+
+});
