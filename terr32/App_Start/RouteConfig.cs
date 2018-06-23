fca1d296f5aed91706d03f360b52847faab5462d
ailo21 <alex92ailo@mail.ru>
2018-06-23 11:14:38 +0300

    index finish

--- terr32/App_Start/RouteConfig.cs
+++ terr32/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace terr32
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller="main",action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ terr32/Controllers/KatalogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace terr32.Controllers
+{
+    public class KatalogController : Controller
+    {
+        // GET: Katalog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
